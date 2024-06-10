df49f2bc24dd9772bbd914218a1939e789000c41
Rajabrahmam-Thanniru <raja9392t@gmail.com>
2024-06-10 16:09:56 +0530

    ..

--- lib/screens/user/orders.dart
+++ lib/screens/user/orders.dart
@@ -1,13 +1,14 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
-class Order extends StatefulWidget {
-  const Order({super.key});
+class Order_screen extends StatefulWidget {
+  const Order_screen({super.key});
 
   @override
-  State<Order> createState() => _OrderState();
+  State<Order_screen> createState() => _OrderState();
 }
 
-class _OrderState extends State<Order> {
+class _OrderState extends State<Order_screen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
--- lib/screens/user/user_home.dart
+++ lib/screens/user/user_home.dart
@@ -5,6 +5,8 @@ import 'package:bite_box/screens/user/profile.dart';
 import 'package:bite_box/screens/user/search.dart';
 import 'package:bite_box/utils/Notifications.dart';
 import 'package:bite_box/utils/cart.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
@@ -109,7 +111,7 @@ class _User_homeState extends State<User_home> {
         children: [
           MainUserHome(),
           Search(),
-          Order(),
+          Order_screen(),
           Liked(),
           Profile(),
         ],
@@ -144,7 +146,9 @@ class _User_homeState extends State<User_home> {
         selectedItemColor: Colors.amber[600],
         onTap: _onItemTapped,
       ),
-      endDrawer: Drawer(),
+      drawer: CustomDrawer(
+        handleNavigation: handleNavigation,
+      ),
     );
   }
 
@@ -168,5 +172,221 @@ class _User_homeState extends State<User_home> {
           break;
       }
     });
+  }
+}
+
+class CustomDrawer extends StatelessWidget {
+  final Function(int) handleNavigation;
+
+  CustomDrawer({required this.handleNavigation});
+  // ignore: non_constant_identifier_names
+  final User = FirebaseAuth.instance.currentUser;
+  Future<String?> getUsername() async {
+    String? username;
+    try {
+      var userDocument = await FirebaseFirestore.instance
+          .collection('Users')
+          .doc(User!.email)
+          .collection('userinfo')
+          .doc('userinfo')
+          .get();
+
+      if (userDocument.exists) {
+        // If the document exists, retrieve the username
+        username = userDocument.get('fullName');
+      }
+    } catch (e) {
+      print("Error getting username: $e");
+    }
+    return username;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final width = MediaQuery.of(context).size.width;
+
+    return Drawer(
+      width: width * 0.80,
+      child: FutureBuilder<String?>(
+        future: getUsername(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Container();
+          } else if (snapshot.hasError) {
+            return Text("Error: ${snapshot.error}");
+          } else {
+            return ListView(
+              padding: EdgeInsets.zero,
+              children: [
+                DrawerHeader(
+                  decoration: BoxDecoration(
+                    color: Colors.black,
+                  ),
+                  child: Stack(
+                    children: [
+                      Positioned(
+                        top: -20,
+                        right: 0,
+                        child: InkWell(
+                          onTap: () {
+                            Navigator.pop(context);
+                          },
+                          child: Container(
+                            padding: EdgeInsets.all(16.0),
+                            child: FaIcon(
+                              FontAwesomeIcons.close,
+                              color: Colors.white,
+                            ),
+                          ),
+                        ),
+                      ),
+                      Row(
+                        children: [
+                          GestureDetector(
+                            onTap: () {
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                  builder: (context) => Profile(),
+                                ),
+                              );
+                            },
+                            child: Icon(
+                              Icons.account_circle,
+                              color: Colors.amber,
+                              size: 60,
+                            ),
+                          ),
+                          Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                ' Welcome,',
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontFamily: 'Roboto',
+                                  fontSize: 18,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                              Text(
+                                '  ${snapshot.data}',
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 14,
+                                  fontFamily: 'Roboto',
+                                ),
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+                ListTile(
+                  leading: FaIcon(
+                    FontAwesomeIcons.houseChimney,
+                    color: Colors.black,
+                  ),
+                  title: Text(
+                    'Home',
+                    style: TextStyle(
+                      fontFamily: 'Roboto',
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  onTap: () {
+                    handleNavigation(
+                        0); // Navigate to the first page (MainBuilderHome)
+                    Navigator.pop(context);
+                  },
+                ),
+                Divider(
+                  color: Colors.black,
+                  thickness: 0,
+                  indent: 0,
+                  endIndent: 0,
+                ),
+                ListTile(
+                  leading: FaIcon(
+                    FontAwesomeIcons.clipboard,
+                    color: Colors.black,
+                  ),
+                  title: Text(
+                    'Orders',
+                    style: TextStyle(
+                      fontFamily: 'Roboto',
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  onTap: () {
+                    handleNavigation(2);
+                    Navigator.pop(context);
+                  },
+                ),
+                Divider(
+                  color: Colors.black,
+                  thickness: 1,
+                  indent: 0,
+                  endIndent: 0,
+                ),
+                ListTile(
+                  leading: FaIcon(
+                    FontAwesomeIcons.cartShopping,
+                    color: Colors.black,
+                  ),
+                  title: Text(
+                    'My Cart',
+                    style: TextStyle(
+                      fontFamily: 'Roboto',
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  onTap: () {
+                    handleNavigation(3);
+                    Navigator.pop(context);
+                  },
+                ),
+                Divider(
+                  color: Colors.black,
+                  thickness: 1,
+                  indent: 0,
+                  endIndent: 0,
+                ),
+                ListTile(
+                  leading: FaIcon(
+                    FontAwesomeIcons.solidHeart,
+                    color: Colors.black,
+                  ),
+                  title: Text(
+                    'Wishlist',
+                    style: TextStyle(
+                      fontFamily: 'Roboto',
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => Liked(),
+                      ),
+                    );
+                  },
+                ),
+                Divider(
+                  color: Colors.black,
+                  thickness: 1,
+                  indent: 0,
+                  endIndent: 0,
+                ),
+              ],
+            );
+          }
+        },
+      ),
+    );
   }
 }
